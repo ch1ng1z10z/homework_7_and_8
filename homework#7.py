caf74524354052ca750a2e24da32019ffcad81cf
ch1ng1z10z <ggckam228@mail.ru>
2025-08-28 23:57:55 +0600

    homework#7 added

--- homework#7.py
+++ homework#7.py
@@ -17,14 +17,47 @@ cursor.execute('''
 ''')
 connect.commit()
 
-# CRUD - Create Read Update Delete
 
-def add_user(name_1, age_1, hobby_1):
-    # cursor.execute(
-    #     "INSERT INTO users(name, age, hobby) VALUES(?, ?, ?)",
-    #     (name_1, age_1, hobby_1)
-    # )
+def sozdanie_user(name_1, age_1, hobby_1):
     cursor.execute(f'INSERT INTO users(name, age, hobby) VALUES("{name_1}", "{age_1}", "{hobby_1}")')
     connect.commit()
     print('Пользователь добавлен!!')
-add_user('Андрей', 15, "Летать")
+sozdanie_user('Анна', 23, "красить")
+sozdanie_user('Николай', 58, "спать")
+sozdanie_user('венник', 2, "убирать")
+sozdanie_user('Уролог', 34, "смотреть")
+sozdanie_user('Венерена', 400000, "перемещяться")
+
+def get_users():
+    cursor.execute('SELECT name, age, hobby FROM users')
+    users = cursor.fetchall()
+
+    for i in users:
+        print(f"ИМЯ: {i[0]}, ВОЗРАСТ: {i[1]}, ХОББИ: {i[2]}")
+
+
+
+def update_user(name, row_id):
+    cursor.execute(f'UPDATE users SET name = "{name}" WHERE rowid = "{row_id}"')
+    connect.commit()
+    print('Пользователь обновлён')
+
+update_user("Macho_men", 14)
+
+
+def delete_user(row_id):
+
+    cursor.execute(f'DELETE FROM users WHERE id = "{row_id}"')
+    connect.commit()
+    print('Пользователь удален!')
+
+delete_user(4)
+
+get_users()
+
+def get_user_by_id(user_id):
+    if cursor.execute(f'SELECT name, age, hobby FROM users WHERE id = {user_id}'):
+        return print(get_user_by_id(2))
+    else:
+        return print(f'Пользователь не найден')
+
